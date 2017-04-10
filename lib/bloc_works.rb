8ebce980b6569e72179801d1cceaa532b389d499
Luther <lutheroto@gmail.com>
2017-04-10 17:03:36 -0400

    Middle of checkpoint 2.

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -1,9 +1,21 @@
 require "bloc_works/version"
+require("bloc_works/dependencies")
+require("bloc_works/controller")
+
+# The following are not specified by the checkpoint.
+#require('bloc_works/router')
+#require('bloc_works/utility')
 
 module BlocWorks
   class Application
     def call env
-      [200, {'Content-Type' => 'text/html'}, ['Hello Blocheads!']]
+      [
+       200,
+       {'Content-Type' => 'text/html'},
+       # I'm changing the message here to show that this is coming from
+       # BlocWorks, not the client app.
+       ["This is BlocWorks! Your app isn't doing anything."],
+      ]
     end
   end
 end
